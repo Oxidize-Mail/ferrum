9c73723477017ca99e94d8220e892198787c1fcd
Jascha <75188000+Jasch-M@users.noreply.github.com>
2026-09-20 21:05:22 +0200

    added a discord bot command (#23)
    * chore(main): remove useless semicolons in lambdas
    
    * feat: added a make-a-difference command
    
    * feat: registered the make-a-difference command
    
    * chore: fixed a typo
    
    ---------
    
    Co-authored-by: Jasch-M <jaschdevelopment@gmail.com>

--- main.cpp
+++ main.cpp
@@ -64,6 +64,13 @@ int main() {
     }
   });
 
+  bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+    if (event.command.get_command_name() == "make-a-difference") {
+      event.reply("If you ever wanted to make a difference in compilers of the world, "
+                  "then all you need to do is join up and become a SoftOmni contributor");
+    }
+  });
+
   bot.on_ready([&bot](const dpp::ready_t& event) {
     std::cout << "Event: " << event.shard_id << " is ready." << std::endl;
     if (dpp::run_once<struct register_bot_commands>()) {
@@ -77,6 +84,9 @@ int main() {
                                server_id);
       bot.guild_command_create(
           dpp::slashcommand("high-praise", "give high praise", bot.me.id),
+          server_id);
+      bot.guild_command_create(
+        dpp::slashcommand("make-a-difference", "Make a difference", bot.me.id),
           server_id);
     }
   });
